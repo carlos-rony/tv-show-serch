20f6cacb73468f3058cf1a84159371d5a7d2a417
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-06-09 21:34:50 -0300

    tarefas

---
+++ index_do_template.js
@@ -0,0 +1,27 @@
+const handleSearch = async (event) => {
+  event.preventDefault();
+
+  // implemente a consulta a partir daqui
+
+  //// Exemplo de endpoint: https://api.tvmaze.com/search/shows?q=lost
+
+  //// Elementos de leiaute importantes:
+
+  //  #message: use para exibir mensagens aos usuário, por exemplo:
+
+  const message = document.querySelector('#message');
+  message.innerHTML = 'Série não encontrada';
+
+  //  #shows: conterá os shows, cada um em um <li>, por exemplo:
+  // <li>
+  //   <img class="poster" src="https://static.tvmaze.com/uploads/images/medium_portrait/0/1389.jpg" />
+  //   <span class="show-name">Lost</span>
+  // </li>
+  let resultado = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .querySelector('#search-form')
+    .addEventListener('submit', handleSearch);
+});
